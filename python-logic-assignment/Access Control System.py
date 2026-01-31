4ffd68d69e79dfd03c3f956a92e08db2f571b1f5
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-01-31 12:19:50 +0530

    access control system assignment submission

--- python-logic-assignment/Access Control System.py
+++ python-logic-assignment/Access Control System.py
@@ -1,1 +1,15 @@
+age = int(input("Enter your age: "))
+has_ID = input("Have ID ?: ").strip().lower()
 
+if has_ID == "true":
+    has_ID = True
+elif has_ID == "false":
+    has_ID = False
+else:
+    print("Invalid input for ID. Please enter either True or False.")
+    exit()
+
+if age >=18 and has_ID:
+    print("Entry allowed")
+else:
+    print("entry not allowed")
