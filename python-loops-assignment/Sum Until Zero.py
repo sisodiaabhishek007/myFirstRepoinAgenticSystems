375414c82fa7a04fd9179c9a022172c103d17014
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-01-31 20:22:04 +0530

    Sum until zero submission

--- python-loops-assignment/Sum Until Zero.py
+++ python-loops-assignment/Sum Until Zero.py
@@ -1,1 +1,13 @@
+#Create a program that keeps asking the user to enter numbers and adds them to a total. When the user enters 0, stop taking input and print the final sum.
+
+sum = 0
+
+while True:
+    num = int(input())
+    if num == 0:
+        break
+    sum += num
+    
+print(sum)    
+
 
