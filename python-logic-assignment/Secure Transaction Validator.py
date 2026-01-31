d7331c9f3c3e3f95dc4c4d3a79f532506c576c28
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-01-31 12:32:13 +0530

    Secure transaction validator file submission

--- python-logic-assignment/Secure Transaction Validator.py
+++ python-logic-assignment/Secure Transaction Validator.py
@@ -1,1 +1,16 @@
+Balance = int(input("Enter balance amount: "))
+Withdarawal = int(input("Enter amount to withdraw: "))
+is_verified = input("Is user verified?: ").strip().lower()
 
+if is_verified == "true":
+    is_verified = True
+elif is_verified == "false":
+    is_verified = False
+else:
+    print("Invalid verifcation input.")
+    exit()
+
+if is_verified and Withdarawal<=Balance and Withdarawal > 0:
+    print("Withdrawal successful.")
+else:
+    print("Transaction denied.")
